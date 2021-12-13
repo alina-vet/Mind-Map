4d40576f4fa665c2281ebf19cbf59f9992960587
Alina Bondarchuk <al.vozhzhova@gmail.com>
2021-12-13 12:08:21 +0200

    v

--- mindMap/Controllers/MindMapViewController.swift
+++ mindMap/Controllers/MindMapViewController.swift
@@ -7,43 +7,82 @@
 
 import UIKit
 
-class MindMapViewController: UIViewController, UIGestureRecognizerDelegate, CardViewDelegate {
-
+class MindMapViewController: UIViewController, UIGestureRecognizerDelegate {
     
-
-    @IBOutlet weak var mainIdeaView: UIView!
-    @IBOutlet weak var button: UIButton!
-    @IBOutlet weak var mainIdeaLabel: UILabel!
     
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var contentView: UIView!
+    
+    var cards = [CardView]()
     var mindIdea: String?
-    //var otherIdeaLabels = [UILabel]()
     var selectedCard: CardView?
-    var xPos: CGFloat = 20.0
+    var mindCard: CardView?
+    var subCard: CardView?
         
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if let labelToLoad = mindIdea {
-            mainIdeaLabel.text = labelToLoad
+            loadCards(CGPoint(x: view.center.x - 50, y: view.center.y), andText: labelToLoad, withColor: .white, withBackColor: UIColor(named: "mindCardColor")!)
         }
         
         let tap = UITapGestureRecognizer(target: self, action: #selector(didTap(_:)))
         view.addGestureRecognizer(tap)
+        
+        
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if self.isMovingToParent {
+            let file = "\(UUID().uuidString)"
+            
+            let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+            let fileURL = dir.appendingPathComponent(file)
+        }
+    }
     
-    //MARK: - CardViewDelegate
+//MARK: - Helper Methods
+    
+    @objc func didTap(_ gesture: UITapGestureRecognizer) {
+        print("did tap")
+        if selectedCard != nil {
+            let tapPoint = gesture.location(in: self.contentView)
+            loadCards(tapPoint, andText: "New idea", withColor: .systemGray, withBackColor: UIColor(named: "cardColor")!)
+            guard let lastLoadCard = cards.last else { return }
+            //let lastLoadCard = cards.removeLast()
+            let line = LineView(from: selectedCard!, to: lastLoadCard)
+            contentView!.insertSubview(line, at: 0)
+            selectedCard?.lines.append(line)
+            lastLoadCard.lines.append(line)
+            selectedCard?.deselected()
+            selectedCard = nil
+            
+        } else {
+            self.view.endEditing(true)
+            for card in cards {
+                card.cardField.isUserInteractionEnabled = false
+            }
+        }
+    }
+    
+    func loadCards(_ atPoint: CGPoint, andText text: String, withColor textColor: UIColor, withBackColor color: UIColor) {
+        let card = CardView(atPoint)
+        card.cardField.text = text
+        card.cardView.backgroundColor = color
+        card.cardField.textColor = textColor
+        card.delegate = self
+        self.contentView.addSubview(card)
+        cards.append(card)
+    }
+    
+}
+
+extension MindMapViewController: CardViewDelegate {
     
     func didSelect(_ card: CardView) {
         if selectedCard != nil {
-            if card == selectedCard {
-                
-            } else {
-                let line = LineView(from: selectedCard!, to: card)
-                view!.insertSubview(line, at: 0)
-                selectedCard?.lines.append(line)
-                card.lines.append(line)
-            }
             selectedCard?.deselected()
             selectedCard = nil
         } else {
@@ -53,58 +92,20 @@ class MindMapViewController: UIViewController, UIGestureRecognizerDelegate, Card
     }
     
     func didEdit(_ card: CardView) {
-        let ac = UIAlertController(title: "Enter new name", message: nil, preferredStyle: .alert)
-                ac.addTextField { textField in
-                    textField.keyboardType = .default
-                    textField.clearButtonMode = .whileEditing
-                    textField.textAlignment = .center
-                    textField.text = card.cardLabel.text
-                }
-                ac.addAction(UIAlertAction(title: "Rename", style: .default, handler: { (action) in
-                    let textField = ac.textFields![0] as UITextField
-                    card.cardLabel.text = textField.text
-                }))
-        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-        self.present(ac, animated: true, completion: nil)
+        print("Edit")
+        card.cardField.isUserInteractionEnabled = true
+        card.cardField.becomeFirstResponder()
+//        let ac = UIAlertController(title: "Enter new name", message: nil, preferredStyle: .alert)
+//        ac.addTextField { textField in
+//            textField.clearButtonMode = .whileEditing
+//            textField.textAlignment = .center
+//            textField.text = card.cardField.text
+//        }
+//        ac.addAction(UIAlertAction(title: "Rename", style: .default, handler: { action in
+//            let textField = ac.textFields![0] as UITextField
+//            card.cardField.text = textField.text
+//        }))
+//        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+//        self.present(ac, animated: true, completion: nil)
     }
-    
-    @objc func didTap(_ gesture: UITapGestureRecognizer) {
-        print("did tap")
-        if selectedCard != nil {
-            selectedCard?.deselected()
-            selectedCard = nil
-        } else {
-            let tapPoint = gesture.location(in: self.view)
-            let card = CardView(tapPoint)
-            card.delegate = self
-            view!.addSubview(card)
-        }
-    }
-    
-    
-//    @IBAction func addNewIdea(_ sender: Any) {
-//        createL()
-//        print("dbcsd")
-//    }
-    //MARK: Helper Methods
-    
-    func createL() {
-        let cardView = CardView(frame: CGRect(x: xPos, y: mainIdeaLabel.center.y + 70, width: 180, height: 100))
-        self.view.addSubview(cardView)
-    }
-        
-
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
-
