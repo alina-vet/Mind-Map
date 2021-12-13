375d6de636078b96702e1b73b52612357d56e92f
Alina Bondarchuk <al.vozhzhova@gmail.com>
2021-12-13 12:17:32 +0200

    v

--- CardView.swift
+++ CardView.swift
@@ -2,7 +2,7 @@
 //  CardView.swift
 //  mindMap
 //
-//  Created by Алина Бондарчук on 10.11.2021.
+//  Created by Алина Бондарчук on 09.12.2021.
 //
 
 import UIKit
@@ -12,22 +12,25 @@ protocol CardViewDelegate {
     func didEdit (_ card: CardView)
 }
 
-class CardView: UIView {
+class CardView: UIView, UITextFieldDelegate {
 
     @IBOutlet var cardView: UIView!
-    @IBOutlet weak var cardLabel: UILabel!
+    @IBOutlet weak var cardField: UITextField!
     
     var lines = [LineView]()
     var delegate: CardViewDelegate?
-    
-    var xPos: CGFloat = 20.0
+    var mindCard: MindMapViewController?
     
     init(_ atPoint: CGPoint) {
         let size: CGFloat = 80
-        let frame = CGRect(x: atPoint.x-size/2, y: atPoint.y-size/2, width: 180, height: 100)
+        let frame = CGRect(x: atPoint.x - size / 2, y: atPoint.y  - size / 2, width: 180, height: 100)
         super.init(frame: frame)
         
-        commonInit()
+        initViewFromXib()
+        
+        cardView.isUserInteractionEnabled = true
+        let interaction = UIContextMenuInteraction(delegate: self)
+        cardView.addInteraction(interaction)
         
         let pan = UIPanGestureRecognizer(target: self, action: #selector(didPanInCard(_:)))
         self.addGestureRecognizer(pan)
@@ -39,6 +42,8 @@ class CardView: UIView {
         let tap = UITapGestureRecognizer(target: self, action: #selector(didTapInCard(_:)))
         tap.require(toFail: doubleTap)
         self.addGestureRecognizer(tap)
+        
+        cardField.delegate = self
     }
     
     required init?(coder: NSCoder) {
@@ -47,24 +52,18 @@ class CardView: UIView {
     
 //MARK: - Init From Xib
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        commonInit()
-    }
-    
-    func commonInit() {
+    func initViewFromXib() {
         let viewFromXib = Bundle.main.loadNibNamed("CardView", owner: self, options: nil)?.first as! UIView
-        viewFromXib.frame = self.bounds
         addSubview(viewFromXib)
     }
     
 //MARK: - Gestures
     
     @objc func didPanInCard(_ gesture: UIPanGestureRecognizer) {
-        print("didPan")
+        print("did drug card")
         if gesture.state == .changed {
             self.center = gesture.location(in: superview)
-            //Uppdatera alla linjer
+            
             for line in lines {
                 line.update()
             }
@@ -74,7 +73,7 @@ class CardView: UIView {
     }
     
     @objc func didTapInCard(_ gesture: UITapGestureRecognizer) {
-        print("did tap in bubble")
+        print("did tap in card")
         guard  let card = gesture.view as? CardView else { return }
         if delegate != nil {
             delegate!.didSelect(card)
@@ -82,7 +81,7 @@ class CardView: UIView {
     }
     
     @objc func didDoubleTapInCard(_ gesture: UITapGestureRecognizer) {
-        print("did double tap in bubble")
+        print("did double tap in card")
         guard  let card = gesture.view as? CardView else { return }
         if delegate != nil {
             delegate!.didEdit(card)
@@ -92,7 +91,7 @@ class CardView: UIView {
 //MARK: - Helper Methods
     
     func selected() {
-        cardView.layer.borderWidth = 10
+        cardView.layer.borderWidth = 5
         cardView.layer.borderColor = UIColor.lightGray.cgColor
     }
     
@@ -100,11 +99,27 @@ class CardView: UIView {
         cardView.layer.borderWidth = 0
     }
     
-//    func delete() {
-//        for line in lines {
-//            line.removeFromSuperview()
-//        }
-//        removeFromSuperview()
+    func deleteTapped() {
+        for line in lines {
+            line.removeFromSuperview()
+        }
+        removeFromSuperview()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        textField.isUserInteractionEnabled = false
+        return true
+    }
+    
+//MARK: - Notifocations
+    
+//    func registerForKeyboardNotifocations() {
+//        NotificationCenter.default.addObserver(self, selector: #selector(kbWillShow), name: NSNotification.Name., object: <#T##Any?#>)
+//    }
+//
+//    @objc func kbWillShow() {
+//
 //    }
     
 }
@@ -120,3 +135,28 @@ extension UIView {
         }
     }
 }
+
+extension CardView: UIContextMenuInteractionDelegate {
+    func contextMenuInteraction(_ interaction: UIContextMenuInteraction, configurationForMenuAtLocation location: CGPoint) -> UIContextMenuConfiguration? {
+        let configuration = UIContextMenuConfiguration(identifier: nil, previewProvider: nil) { actions -> UIMenu? in
+            let rename = UIAction(title: "Rename", image: UIImage(systemName: "square.and.pencil"), handler: { [self] _ in
+                
+                cardField.isUserInteractionEnabled = true
+                cardField.resignFirstResponder()
+                cardField.becomeFirstResponder()
+            })
+            
+            let delete = UIAction(title: "Delete", image: UIImage(systemName: "trash"), attributes: .destructive, handler: { [self] _ in
+                deleteTapped()
+            })
+            
+            return  UIMenu(title: "Edit", image: nil, identifier: nil, options: [], children: [rename, delete])
+        }
+        return configuration
+    }
+}
+
+/*
+ guard let touch = touches.first else { return }
+ let location = touch.location(in: view)
+ */
